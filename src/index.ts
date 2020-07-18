4d367ceb6bd9fd0f8458a17b35cfbea072553dd6
Aaron Goldsmith <aarongoldsmith1@outlook.com>
2020-07-18 15:45:36 -0700

    add user maker method

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,7 @@ import { Company } from './Company';
 
 import { CustomMap } from './CustomMap';
 
+const user = new User();
 const customMap = new CustomMap('map');
+
+customMap.addUserMarker(user);
